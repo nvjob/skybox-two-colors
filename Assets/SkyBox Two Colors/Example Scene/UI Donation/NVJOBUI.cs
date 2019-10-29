444395c96528a0b309bca0fa8f16220c1d9f6881
#NVJOB Nicholas Veselov <reset86@gmail.com>
2019-10-30 01:22:43 +0300

    v1.2
    Minor

--- Assets/SkyBox Two Colors/Example Scene/UI Donation/NVJOBUI.cs
+++ Assets/SkyBox Two Colors/Example Scene/UI Donation/NVJOBUI.cs
@@ -1,6 +1,7 @@
 ﻿// #NVJOB Nicholas Veselov
 
 
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,40 +15,86 @@ public class NVJOBUI : MonoBehaviour
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+    GameObject logo, donation, report;
+    static WaitForSeconds delay0 = new WaitForSeconds(4.0f);
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
     void Awake()
     {
         //---------------------------------
 
-        transform.Find("Panel/logo").GetComponent<Button>().onClick.AddListener(Logo);
-        transform.Find("Panel/donation").GetComponent<Button>().onClick.AddListener(Donation);
+        logo = transform.Find("Panel/logo").gameObject;
+        donation = transform.Find("Panel/donation").gameObject;
+        report = transform.Find("Panel/report").gameObject;
+        
+        logo.GetComponent<Button>().onClick.AddListener(Logo);
+        donation.GetComponent<Button>().onClick.AddListener(Donation);
+        report.GetComponent<Button>().onClick.AddListener(Report);
+
+        StartCoroutine("_Rotation");
 
         //---------------------------------
     }
-    
+
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    
+
+
+    IEnumerator _Rotation()
+    {
+        //--------------
+
+        while (true)
+        {
+            logo.SetActive(true);
+            donation.SetActive(false);
+            report.SetActive(false);
+            yield return delay0;
+            logo.SetActive(false);
+            donation.SetActive(true);
+            report.SetActive(false);
+            yield return delay0;
+            logo.SetActive(false);
+            donation.SetActive(false);
+            report.SetActive(true);
+            yield return delay0;
+        }
+
+        //--------------
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
     void Logo()
     {
-        //---------------------------------
+        //--------------
 
         Application.OpenURL("https://nvjob.github.io/unity/skybox-two-colors");
 
-        //---------------------------------
+        //--------------
     }
-
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    
 
     void Donation()
     {
-        //---------------------------------
+        //--------------
 
         Application.OpenURL("https://nvjob.github.io/donate");
 
-        //---------------------------------
+        //--------------
+    }
+
+    void Report()
+    {
+        //--------------
+
+        Application.OpenURL("https://nvjob.github.io/reportaproblem/");
+
+        //--------------
     }
 
 
